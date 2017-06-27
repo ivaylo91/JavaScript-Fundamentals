6d20ee4967b8787ef82bfb340324dba25b36a3d6
ivaylo91 <ipenev91@gmail.com>
2017-06-27 17:35:44 +0300

    Sum Three Numbers Task Finished and ./idea folder from repository.

---
+++ JavaScriptSyntax/sum.js
@@ -0,0 +1,12 @@
+/**
+ * @author Ivaylo Penev
+ */
+
+const sumThreeNumbers = function (input) {
+
+    let sum = input[0] + input[1] + input[2];
+
+    return sum;
+};
+
+console.log(sumThreeNumbers([2, 3, 4])); // 9
