efc784eff10f450cc0c7ab215a13382dc59b72c8
ivaylo91 <ipenev91@gmail.com>
2017-06-30 16:55:49 +0300

    Multiply Two Numbers function finished.

---
+++ OperatorsAndLogicFlow/MultiplyNumbers/multiply.js
@@ -0,0 +1,10 @@
+/**
+ * @author Ivaylo Penev
+ */
+const multiplyNumbers = function (a,b) {
+
+    return a * b;
+};
+
+console.log(multiplyNumbers(2,3));
+console.log(multiplyNumbers(23632.36,-12.3249));
