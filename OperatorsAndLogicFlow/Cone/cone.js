4d51082e588c29fafcc6569706d5c19117d543b4
ivaylo91 <ipenev91@gmail.com>
2017-06-30 21:51:55 +0300

    Cone Volume and Area calc function finished.

--- OperatorsAndLogicFlow/Cone/cone.js
+++ OperatorsAndLogicFlow/Cone/cone.js
@@ -11,4 +11,5 @@ const calcCone = function (radius,height) {
     return 'area =' + area.toFixed(4) + '\n' + 'volume='+ volume.toFixed(4);
 };
 
-console.log(calcCone(3,5));
+console.log(calcCone(3,5)); // 83.2298 , 47.1239
+console.log(calcCone(3.3,7.8)); // 122.0159 , 889511
