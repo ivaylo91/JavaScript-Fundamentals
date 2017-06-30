2d18b3b7808ed4cdd1285b4a49625fe01019458c
ivaylo91 <ipenev91@gmail.com>
2017-06-30 17:14:35 +0300

    Boxes and Bottles function finished and change README file.

--- OperatorsAndLogicFlow/MultiplyNumbers/multiply.js
+++ OperatorsAndLogicFlow/MultiplyNumbers/multiply.js
@@ -6,5 +6,5 @@ const multiplyNumbers = function (a,b) {
     return a * b;
 };
 
-console.log(multiplyNumbers(2,3));
-console.log(multiplyNumbers(23632.36,-12.3249));
+console.log(multiplyNumbers(2,3)); //6
+console.log(multiplyNumbers(23632.36,-12.3249)); // -291266.473764
