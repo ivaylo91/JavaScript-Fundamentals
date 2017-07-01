7821bbe55ad81ccf9c1eedb04d97b7a1446cc33c
ivaylo91 <ipenev91@gmail.com>
2017-07-01 21:21:47 +0300

    Even Or Odd check function is finished.

---
+++ OperatorsAndLogicFlow/EvenOrOdd/evenOrOdd.js
@@ -0,0 +1,20 @@
+/**
+ * @author Ivaylo Penev
+ */
+
+const checkEvenOrOdd = function (input) {
+    let  number = Number(input)
+    if(number % 2 === 0){
+        return 'even';
+    } else if( number % 2 === Math.round(number%2)){
+
+        return 'odd';
+    } else {
+        return 'invalid';
+    }
+
+};
+
+console.log(checkEvenOrOdd(5)); //odd
+console.log(checkEvenOrOdd(8));//even
+console.log(checkEvenOrOdd(1.5)); //invalid
