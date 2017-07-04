c2b80d5404ae39778a9f7c630f0b381505ed576b
ivaylo91 <ipenev91@gmail.com>
2017-07-04 20:02:27 +0300

    Second Variant of ChessBoard Draw

---
+++ OperatorsAndLogicFlow/ChessBoard/chessBoard.js
@@ -0,0 +1,29 @@
+/**
+ * @author Ivaylo Penev
+ */
+
+const drawChessBoard = function (n) {
+
+    let board = '<div class="chessboard">\n';
+
+    let color = 'black';
+
+    for (let i = 1; i <= n; i++) {
+
+        board += '<div>\n';
+
+        for (let j = 1; j <= n; j++) {
+
+            if (i % 2 === j % 2) {
+                color = 'black';
+            } else {
+                color = "white";
+            }
+            board += `<span class="${color}"></span>\n`;
+        }
+        board += '</div>\n';
+    }
+    board += '</div>';
+    return board;
+};
+console.log(drawChessBoard(3));
