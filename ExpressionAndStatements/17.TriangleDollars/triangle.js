07aae622366454451c596275e62f47896b605735
ivaylo91 <ipenev91@gmail.com>
2017-07-10 10:41:23 +0300

    Draw Trinagle dollars symbol function

---
+++ ExpressionAndStatements/17.TriangleDollars/triangle.js
@@ -0,0 +1,20 @@
+/**
+ * @author Ivaylo Penev
+ */
+
+const drawTriangle = function (n) {
+
+    let line = '';
+
+    for (let i = 1; i <= n; i++) {
+
+        line += '$';
+
+        console.log(line);
+    }
+
+};
+
+drawTriangle(3);
+
+
