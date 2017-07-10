c46e6c2323018d9c136f437aa82ee3c9a19f601e
ivaylo91 <ipenev91@gmail.com>
2017-07-10 10:34:45 +0300

    check points inside or outside rectangle function

---
+++ ExpressionAndStatements/16.OddNumbers1toN/oddNumbersPrint.js
@@ -0,0 +1,15 @@
+/**
+ * @author Ivaylo Penev
+ */
+
+const printOddNumbers = function (n) {
+
+    for (let i = 1; i <= n; i += 2) {
+
+        console.log(i);
+    }
+};
+
+printOddNumbers(7);
+
+
