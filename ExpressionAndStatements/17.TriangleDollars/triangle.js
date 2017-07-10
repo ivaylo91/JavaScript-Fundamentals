fbe6f71074c4ad79d2d2300d401b5ddf3bb4e42c
ivaylo91 <ipenev91@gmail.com>
2017-07-10 15:11:05 +0300

    Draw Triangle with $ symbol function

--- ExpressionAndStatements/17.TriangleDollars/triangle.js
+++ ExpressionAndStatements/17.TriangleDollars/triangle.js
@@ -4,15 +4,26 @@
 
 const drawTriangle = function (n) {
 
-    let line = '';
+    /* let line = '';
 
-    for (let i = 1; i <= n; i++) {
+     for (let i = 1; i <= n; i++) {
 
-        line += '$';
+     line += '$';
 
-        console.log(line);
+     console.log(line);
+     }*/
+
+    /*  for (let row = 1; row <= n; row++) {
+
+     console.log(new Array(row + 1).join('$'));
+     }
+
+     */
+
+    for (let row = 1; row <= n; row++) {
+
+        console.log('$'.repeat(row));
     }
-
 };
 
 drawTriangle(3);
