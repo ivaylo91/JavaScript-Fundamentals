5fb67972a71bac3bed411a0916c67e23955105e0
ivaylo91 <ipenev91@gmail.com>
2017-07-21 11:37:16 +0300

    Process odd index number in array function.

--- 05.ArraysAndMatrices/05.ProcessNumber/process.js
+++ 05.ArraysAndMatrices/05.ProcessNumber/process.js
@@ -4,17 +4,24 @@
 
 const getNumbers = function (input) {
 
-    let result = [];
+    /*    let result = [];
 
-    for (let i = 0; i < input.length; i++) {
+        for (let i = 0; i < input.length; i++) {
 
-        if (i % 2 !== 0) {
+            if (i % 2 !== 0) {
 
-            result.push(Number(input[i]) * 2);
-            result.reverse();
+                result.push(Number(input[i]) * 2);
+                result.reverse();
+            }
         }
-    }
-    return result.join(' ');
+        return result.join(' ');*/
+
+    let result = input.filter(e => e % 2 !== 0)
+        .map(e => e * 2)
+        .reverse()
+        .join(' ');
+
+    return result;
 };
 
 console.log(getNumbers([10, 15, 20, 25]));
