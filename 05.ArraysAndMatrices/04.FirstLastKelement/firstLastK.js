8e832bf6cbdbe70c9c105bcc30aaabe23af50f4f
ivaylo91 <ipenev91@gmail.com>
2017-07-21 10:53:25 +0300

    First Last K element function

--- 05.ArraysAndMatrices/04.FirstLastKelement/firstLastK.js
+++ 05.ArraysAndMatrices/04.FirstLastKelement/firstLastK.js
@@ -4,33 +4,10 @@
 
 function firstLastK(input) {
 
-    let k = Number(input[0]);
+    let k = input.shift();
 
-    function firstK(arr) {
-
-        let resultArr = [];
-
-        for (let i = 1; i <= k; i++) {
-
-            resultArr.push(arr[i]);
-        }
-        return resultArr.toString();
-    }
-
-    function lastK(arr) {
-
-        let resultArr = [];
-
-        for (let i = arr.length - k; i < arr.length; i++) {
-
-            resultArr.push(arr[i]);
-        }
-
-        return resultArr.toString();
-    }
-
-    console.log(firstK(input));
-    console.log(lastK(input));
+    console.log(input.slice(0, k).join(' '));
+    console.log(input.slice(-k).join(' '));
 }
 
 firstLastK(['2', '7', '8', '9']);
