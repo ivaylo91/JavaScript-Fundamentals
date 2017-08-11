b367a8f3f266739b4743324854d6108734203721
ivaylo91 <ipenev91@gmail.com>
2017-08-11 19:44:19 +0300

    Sort Array in 2 Criteria

---
+++ 05.ArraysAndMatrices/13.SortArray/sort.js
@@ -0,0 +1,21 @@
+const sortArray = function (arr) {
+    console.log(arr.sort(compare).join("\n"));
+
+    function compare(a, b) {
+        if (a.length < b.length) {
+            return -1;
+        } else if (a.length > b.length) {
+            return 1;
+        } else {
+            if (a < b) {
+                return -1;
+            } else if (a > b) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    }
+};
+sortArray(['alpha', 'beta', 'gamma']); // beta alpha gamma
+sortArray(['test', 'Deny', 'omen', 'Default']); // Deny omen test Default
