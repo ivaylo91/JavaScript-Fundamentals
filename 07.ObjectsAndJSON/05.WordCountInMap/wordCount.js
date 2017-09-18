d85159389ac1eeab898a98a583a6d8d6041841eb
ivaylo91 <ipenev91@gmail.com>
2017-09-18 16:40:43 +0300

    word count function with Map and City population calculate function was finished.

---
+++ 07.ObjectsAndJSON/05.WordCountInMap/wordCount.js
@@ -0,0 +1,12 @@
+function countWordsWithMaps(inputLines) {
+    let words = inputLines.join('\n').toLowerCase()
+        .split(/[^A-Za-z0-9_]+/).filter(w => w != '');
+    let wordsCount = new Map();
+    for (let w of words)
+        wordsCount.has(w) ? wordsCount.set(w,
+            wordsCount.get(w) + 1) : wordsCount.set(w, 1);
+    let allWords = Array.from(wordsCount.keys()).sort();
+    allWords.forEach(w =>
+        console.log(`'${w}' -> ${wordsCount.get(w)} times`));
+}
+countWordsWithMaps(['JS and NodeJs','JS again and again','Oh,JS?']);
